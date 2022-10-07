2137b85a52442cabef81e228bd04c95625f3b894
arjuniyer01 <iyer9@wisc.edu>
2022-10-06 20:00:00 -0500

    combined image

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 from constant_values import *
 
-from PIL import Image
 from streamlit_timeline import timeline
 import streamlit.components.v1 as components
 from streamlit_echarts import st_echarts
 
 st.set_page_config(page_title="Arjun Iyer's Resume", layout="wide", page_icon=":smile:")
 
+# div.stDownloadButton > button:hover {{background-color: #00ff00;color:#ff0000;}}
+# div.stDownloadButton > button:hover {{border-color: white;}}
 st.markdown(f'''
     <style>
         section[data-testid="stSidebar"] .css-ng1t4o {{width: 25rem;}}
         section[data-testid="stSidebar"] .css-1d391kg {{width: 25rem;}}
+        div.stDownloadButton > button:first-child {{background-color: #000000; color:#FFFFFF; border-radius: 10px; padding: 10px; font-weight: bold;}}  
+        div.stDownloadButton > button:hover {{border-width: thick;}}
     </style>
 ''',unsafe_allow_html=True)
 
@@ -19,10 +22,24 @@ with st.sidebar:
     components.html(embed_component["linkedin"], height=300)
     components.html(embed_component["github"], height=500)
 
-pdfFileObj = open("pdf_files/iyer_resume.pdf", "rb")
-st.sidebar.download_button("Download PDF Resume", pdfFileObj, file_name="iyer_resume.pdf", mime="pdf")
+# col7, col8 = st.sidebar.columns([1,5])
+# with col7:
+#     pass
+# with col8:
+#     pdfFileObj = open("pdf_files/iyer_resume.pdf", "rb")
+#     st.download_button("Download PDF Resume", pdfFileObj, file_name="iyer_resume.pdf", mime="pdf")
 
-st.markdown("## :email: iyer9@wisc.edu &emsp;&emsp; :phone: 608-622-4320")
+col1, col2, col3 = st.columns([4, 4, 3])
+
+with col1:
+    st.markdown("## :email: iyer9@wisc.edu")
+with col2:
+    st.markdown("## :phone: 608-622-4320")
+with col3 :
+    pdfFileObj = open("pdf_files/iyer_resume.pdf", "rb")
+    st.download_button("Download PDF Resume", pdfFileObj, file_name="iyer_resume.pdf", mime="pdf")
+
+# st.markdown("## :email: iyer9@wisc.edu &emsp;&emsp; :phone: 608-622-4320")
 
 
 with st.spinner(text="Building line"):
@@ -68,14 +85,15 @@ clicked_label = st_echarts(
 )
 
 if clicked_label:
-    if clicked_label == "Python":
-        st.markdown(repository_md_strings['Pneumonia_Detector'])
-        st.markdown(repository_md_strings['resume-website'])
-    if clicked_label == "C":
-        st.markdown(repository_md_strings['neural-network-from-scratch'])
-        st.markdown(repository_md_strings['Memory_Allocator'])
-    if clicked_label == "Java":
-        st.markdown(repository_md_strings['WikipediaTermSearch'])
-        st.markdown(repository_md_strings['BestFlight'])
-    if clicked_label == "R":
-        st.markdown(repository_md_strings['Instant_Body_Fat'])
+    with st.spinner(text="Loading Repositories"):
+        if clicked_label == "Python":
+            st.markdown(repository_md_strings['Pneumonia_Detector'])
+            st.markdown(repository_md_strings['resume-website'])
+        if clicked_label == "C":
+            st.markdown(repository_md_strings['neural-network-from-scratch'])
+            st.markdown(repository_md_strings['Memory_Allocator'])
+        if clicked_label == "Java":
+            st.markdown(repository_md_strings['WikipediaTermSearch'])
+            st.markdown(repository_md_strings['BestFlight'])
+        if clicked_label == "R":
+            st.markdown(repository_md_strings['Instant_Body_Fat'])
