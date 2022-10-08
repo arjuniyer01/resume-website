7111262ef470fc49bf74b209fa731d2c84df2460
arjuniyer01 <iyer9@wisc.edu>
2022-10-07 19:02:40 -0500

    cleaned code

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 import streamlit as st
 from constant_values import *
-
 from streamlit_timeline import timeline
 import streamlit.components.v1 as components
 from streamlit_echarts import st_echarts
+import base64
 
 st.set_page_config(page_title="Arjun Iyer's Resume", layout="wide", page_icon=":smile:")
 
-# div.stDownloadButton > button:hover {{background-color: #00ff00;color:#ff0000;}}
-# div.stDownloadButton > button:hover {{border-color: white;}}
-st.markdown(f'''
-    <style>
-        section[data-testid="stSidebar"] .css-ng1t4o {{width: 25rem;}}
-        section[data-testid="stSidebar"] .css-1d391kg {{width: 25rem;}}
-        div.stDownloadButton > button:first-child {{background-color: #000000; color:#FFFFFF; border-radius: 10px; padding: 10px; font-weight: bold;}}  
-        div.stDownloadButton > button:hover {{border-width: thick;}}
-    </style>
-''',unsafe_allow_html=True)
+st.markdown(page_setup, unsafe_allow_html=True)
 
 with st.sidebar:
     components.html(embed_component["linkedin"], height=300)
     components.html(embed_component["github"], height=500)
-
-# col7, col8 = st.sidebar.columns([1,5])
-# with col7:
-#     pass
-# with col8:
-#     pdfFileObj = open("pdf_files/iyer_resume.pdf", "rb")
-#     st.download_button("Download PDF Resume", pdfFileObj, file_name="iyer_resume.pdf", mime="pdf")
 
 col1, col2, col3 = st.columns([4, 4, 3])
 
@@ -39,61 +23,25 @@ with col3 :
     pdfFileObj = open("pdf_files/iyer_resume.pdf", "rb")
     st.download_button("Download PDF Resume", pdfFileObj, file_name="iyer_resume.pdf", mime="pdf")
 
-# st.markdown("## :email: iyer9@wisc.edu &emsp;&emsp; :phone: 608-622-4320")
-
-
 with st.spinner(text="Building line"):
     with open("timeline.json", "r") as f:
         data = f.read()
         timeline(data, height=500,)
 
-
 st.markdown("# Interactive Project Viewer :computer:")
-st.markdown("#### Click on the chart to view my work in a language!")
 
-option = {
-    "click": "function(params) { console.log(params.name) }",
-    "toolbox": {
-        "show": True,
-        "feature": {
-            "mark": {"show": True},
-        },
-    },
-    "series": [
-        {
-            "type": "pie",
-            "radius": [50, 250],
-            "center": ["50%", "50%"],
-            "roseType": "area",
-            "itemStyle": {"borderRadius": 8},
-            "data": [
-                {"value": 10, "name": "Python"},
-                {"value": 9, "name": "C"},
-                {"value": 8, "name": "Java"},
-                {"value": 7, "name": "TypeScript"},
-                {"value": 8, "name": "SQL"},
-                {"value": 8, "name": "R"},
-                {"value": 7, "name": "Bash"},
-            ],
-        }
-    ],
-}
-clicked_label = st_echarts(
-    options=option, 
-    height="600px",
+st.markdown('<h4><span style="color:blue">Click</span> on the chart to view my work in a language! </h4>', unsafe_allow_html=True)
+st.markdown('<br>', unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+with col1:
+    clicked_label = st_echarts(
+    options=lang_chart, 
+    height="400px",
     events={"click": "function(params) { return params.name }"},
 )
-
-if clicked_label:
-    with st.spinner(text="Loading Repositories"):
-        if clicked_label == "Python":
-            st.markdown(repository_md_strings['Pneumonia_Detector'])
-            st.markdown(repository_md_strings['resume-website'])
-        if clicked_label == "C":
-            st.markdown(repository_md_strings['neural-network-from-scratch'])
-            st.markdown(repository_md_strings['Memory_Allocator'])
-        if clicked_label == "Java":
-            st.markdown(repository_md_strings['WikipediaTermSearch'])
-            st.markdown(repository_md_strings['BestFlight'])
-        if clicked_label == "R":
-            st.markdown(repository_md_strings['Instant_Body_Fat'])
+with col2:
+    if clicked_label:
+        for item in repository_embed[clicked_label]:
+            st.markdown(repository_embed[clicked_label][item])
+        
--- constant_values.py
+++ constant_values.py
@@ -1,39 +1,64 @@
-info = {
-    "name": "Arjun Iyer",
-    "Brief": "Data Scientist",
-    "photo": {"path": "abc.jpg", "width": 150},
-    "Mobile": "608-6224320",
-    "Email": "iyer9@wisc.edu",
-    "City": "Madison, WI",
-    "skills": [
-        "Data Science",
-        "RDBMS",
-        "Python",
-        "Java",
-        "C++",
-        "Airflow",
-        "AWS S3",
-        "Tableau",
-        "Streamlit",
-    ],
-    "achievements": [
-        "Top AI writer @ Medium with 80+ blogs",
-    ],
-}
+page_setup = f'''
+    <style>
+        section[data-testid="stSidebar"] .css-ng1t4o {{width: 25rem;}}
+        section[data-testid="stSidebar"] .css-1d391kg {{width: 25rem;}}
+        div.stDownloadButton > button:first-child {{background-color: #000000; color:#FFFFFF; border-radius: 10px; padding: 10px; font-weight: bold;}}  
+        div.stDownloadButton > button:hover {{border-width: thick;}}
+    </style>
+'''
 
-skill_col_size = 5
 embed_component = {
     "linkedin": """<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
         <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="arjun-iyer-4b6412196" data-version="v1"></div>
               """,
     "github": '<script type="module" src="https://unpkg.com/@rocktimsaikia/github-card@latest?module"></script><div class="github-card" data-width="500"><github-card data-user="arjuniyer01" data-theme="dark" data-height="1000"></github-card></div',
 }
-repository_md_strings = {
-    'Pneumonia_Detector': '[![arjuniyer01/Pneumonia_Detector - GitHub](https://gh-card.dev/repos/arjuniyer01/Pneumonia_Detector.svg)](https://github.com/arjuniyer01/Pneumonia_Detector)',
+
+repository_embed = {
+    'Python' : {
+        'Pneumonia_Detector': '[![arjuniyer01/Pneumonia_Detector - GitHub](https://gh-card.dev/repos/arjuniyer01/Pneumonia_Detector.svg)](https://github.com/arjuniyer01/Pneumonia_Detector)',
     'resume-website': '[![arjuniyer01/resume-website - GitHub](https://gh-card.dev/repos/arjuniyer01/resume-website.svg)](https://github.com/arjuniyer01/resume-website)',
-    'neural-network-from-scratch': '[![arjuniyer01/neural-network-from-scratch - GitHub](https://gh-card.dev/repos/arjuniyer01/neural-network-from-scratch.svg)](https://github.com/arjuniyer01/neural-network-from-scratch)',
-    'WikipediaTermSearch': '[![arjuniyer01/WikipediaTermSearch - GitHub](https://gh-card.dev/repos/arjuniyer01/WikipediaTermSearch.svg)](https://github.com/arjuniyer01/WikipediaTermSearch)',
+    },
+    'C': {
+        'neural-network-from-scratch': '[![arjuniyer01/neural-network-from-scratch - GitHub](https://gh-card.dev/repos/arjuniyer01/neural-network-from-scratch.svg)](https://github.com/arjuniyer01/neural-network-from-scratch)',
+        'Memory_Allocator': '[![arjuniyer01/Memory_Allocator - GitHub](https://gh-card.dev/repos/arjuniyer01/Memory_Allocator.svg)](https://github.com/arjuniyer01/Memory_Allocator)',
+    },
+    'Java': {
+        'WikipediaTermSearch': '[![arjuniyer01/WikipediaTermSearch - GitHub](https://gh-card.dev/repos/arjuniyer01/WikipediaTermSearch.svg)](https://github.com/arjuniyer01/WikipediaTermSearch)',
     'BestFlight': '[![arjuniyer01/BestFlight - GitHub](https://gh-card.dev/repos/arjuniyer01/BestFlight.svg)](https://github.com/arjuniyer01/BestFlight)',
-    'Instant_Body_Fat': '[![arjuniyer01/Instant_Body_Fat - GitHub](https://gh-card.dev/repos/arjuniyer01/Instant_Body_Fat.svg)](https://github.com/arjuniyer01/Instant_Body_Fat)',
-    'Memory_Allocator': '[![arjuniyer01/Memory_Allocator - GitHub](https://gh-card.dev/repos/arjuniyer01/Memory_Allocator.svg)](https://github.com/arjuniyer01/Memory_Allocator)',
-    }
+    },
+    'R': {
+        'Instant_Body_Fat': '[![arjuniyer01/Instant_Body_Fat - GitHub](https://gh-card.dev/repos/arjuniyer01/Instant_Body_Fat.svg)](https://github.com/arjuniyer01/Instant_Body_Fat)',
+    },
+    'Bash': {},
+    'TypeScript': {},
+    'SQL': {},
+}
+
+lang_chart = {
+    "click": "function(params) { console.log(params.name) }",
+    "toolbox": {
+        "show": True,
+        "feature": {
+            "mark": {"show": True},
+        },
+    },
+    "series": [
+        {
+            "type": "pie",
+            "radius": [30, 150],
+            "center": ["50%", "50%"],
+            "roseType": "area",
+            "itemStyle": {"borderRadius": 8},
+            "data": [
+                {"value": 10, "name": "Python"},
+                {"value": 9, "name": "C"},
+                {"value": 8, "name": "Java"},
+                {"value": 7, "name": "TypeScript"},
+                {"value": 8, "name": "SQL"},
+                {"value": 8, "name": "R"},
+                {"value": 7, "name": "Bash"},
+            ],
+        }
+    ],
+}
